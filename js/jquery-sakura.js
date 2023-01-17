19d67a471a817547ffeb312edacb27453da226bd
Boyeon Son <boyeon.son@samsung.com>
2023-01-17 17:12:15 +0900

    Delete some files and add phone number

--- js/jquery-sakura.js
+++ js/jquery-sakura.js
@@ -54,7 +54,7 @@
         var defaults = {
             blowAnimations: ['blow-soft-left', 'blow-medium-left', 'blow-hard-left', 'blow-soft-right', 'blow-medium-right', 'blow-hard-right'],
             className: 'sakura',
-            fallSpeed: 2,
+            fallSpeed: 1.5,
             maxSize: 14,
             minSize: 9,
             newOn: 200,
